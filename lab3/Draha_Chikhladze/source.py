ff3a48d94465eb06a32fe5371b6efed6eb5791ef
d-laslo <ice_bear@ukr.net>
2021-11-16 03:56:50 +0200

    add support functions

--- lab3/Draha_Chikhladze/source.py
+++ lab3/Draha_Chikhladze/source.py
@@ -7,5 +7,27 @@ def write(path, text):
     with open(path, 'w') as file:
         file.write(text)
 
+
+def norm(x, pol):
+    bx = bin(x)[2:]
+    bpol = bin(pol)[2:]
+
+    while len(bx) >= len(bpol):
+        shift = len(bx) - len(bpol)
+        x ^= pol << shift
+        bx = bin(x)[2:]
+    return x
+
+
+def mod(x, pol):
+        bx = bin(x)[2:]
+        bpol = bin(pol)[2:]
+
+        while x >= pol:
+            shift = len(bx) - len(bpol)
+            x ^= pol << shift
+            bx = bin(x)[2:]
+        return x
+
 if __name__ == '__main__':
     pass
