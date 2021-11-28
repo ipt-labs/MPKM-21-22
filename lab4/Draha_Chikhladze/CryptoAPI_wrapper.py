9d8c184641ea855344d2fbc0e82585fe2d05e7f1
d-laslo <ice_bear@ukr.net>
2021-11-28 02:58:39 +0200

    add key deletion

--- lab4/Draha_Chikhladze/CryptoAPI_wrapper.py
+++ lab4/Draha_Chikhladze/CryptoAPI_wrapper.py
@@ -9,38 +9,62 @@ class Wrapper:
 
 
     def generate_private_key(self):
+        self.del_private_key()
+        self.del_public_key()
         self.__private_key = self.__DSTU.private_key
+        
 
 
     def generate_public_key(self):
         if self.__private_key == 0:
             raise Exception()
+        if self.__DSTU.private_key != self.__private_key:
+            self.__DSTU.set_private_key(self.__private_key)
+
+        self.del_public_key()
         self.__public_key = self.__DSTU.public_key
 
 
     def export_private_key(self, path):
         if self.__private_key == 0:
             raise Exception()
-        self.__private_key = write(hex(self.__private_key))
+        self.__private_key = write(path, hex(self.__private_key))
 
 
     def export_public_key(self, path):
         if self.__public_key == 0:
             raise Exception()
-        self.__public_key = write(hex(self.__public_key))
+        self.__public_key = write(path, hex(self.__public_key))
 
 
     def import_private_key(self, path):
+        self.del_private_key()
+        self.del_public_key()
         self.__private_key = int(read(path), 16)
 
 
     def import_public_key(self, path):
+        self.del_public_key()
         self.__public_key = int(read(path), 16)
 
 
     def sign(self, T):
+        if self.__private_key == 0:
+            raise Exception()
         return self.__DSTU.sign(T, self.__private_key)
 
 
     def verify(self, D):
+        if self.__public_key == 0:
+            raise Exception()
         return self.__DSTU.sign(D, self.__public_key)
+
+
+    def del_private_key(self):
+        del self.__private_key
+        self.__private_key = 0
+
+    
+    def del_public_key(self):
+        del self.__public_key
+        self.__public_key = 0
