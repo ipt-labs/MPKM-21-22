00d23e462ae9b66d45296fa06ba1522601af36f6
Anastasiia Moroziuk <lucius2305@gmail.com>
2021-12-20 15:50:36 +0200

    added lab1

---
+++ lab2/Moroziuk_Gtman_Mitrofanova/lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
