ad422f3b01f6b99f504c9b7a3c8ec156d1c564e4
squareKM <90339117+squareKM@users.noreply.github.com>
2021-12-22 00:37:45 +0200

    Add files via upload

---
+++ lab2/Tulupov_Kustareva/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using BankLibrary;
+using System.Security.Cryptography;
+using System;
+
+
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank<Account> bank = new Bank<Account>("Kuna");
+            bool alive = true;
+            while (alive)
+            {
+                ConsoleColor color = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.DarkYellow; 
+                Console.WriteLine("1. Открыть счет \t 2. Вывести средства  \t 3. Добавить на счет");
+                Console.WriteLine("4. Закрыть счет \t 5. Пропустить день \t 6. Выйти из программы \t 7.Осуществить перевод");
+                Console.WriteLine("Введите номер пункта:");
+
+                Console.ForegroundColor = color;
+                try
+                {
+                    int command = Convert.ToInt32(Console.ReadLine());
+
+                    switch (command)
+                    {
+                        case 1:
+                            OpenAccount(app);
+                            break;
+                        case 2:
+                            Withdraw(app);
+                            break;
+                        case 3:
+                            Put(app);
+                            break;
+                        case 4:
+                            CloseAccount(app);
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            alive = false;
+                            continue;
+                        case 7:
+                            Crypto(app);
+                            break;
+                    }
+                    bank.CalculatePercentage();
+                }
+                catch (Exception ex)
+                {
+                    color = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = color;
+                }
+            }
+        }
+
+
+        public abstract class Crypto
+    {
+        public KeyAlgorithm()
+        {
+            byte[] modulus =
+            {
+            214,46,220,83,160,73,40,39,201,155,19,202,3,11,191,178,56,
+            74,90,36,248,103,18,144,170,163,145,87,54,61,34,220,222,
+            207,137,149,173,14,92,120,206,222,158,28,40,24,30,16,175,
+            108,128,35,230,118,40,121,113,125,216,130,11,24,90,48,194,
+            240,105,44,76,34,57,249,228,125,80,38,9,136,29,117,207,139,
+            168,181,85,137,126,10,126,242,120,247,121,8,100,12,201,171,
+            38,226,193,180,190,117,177,87,143,242,213,11,44,180,113,93,
+            106,99,179,68,175,211,164,116,64,148,226,254,172,147
+        };
+
+            byte[] exponent = { 1, 0, 1 };
+
+            byte[] encryptedSymmetricKey;
+            byte[] encryptedSymmetricIV;
+            RSA rsa = RSA.Create();
+
+            RSAParameters rsaKeyInfo = new RSAParameters();
+
+            rsaKeyInfo.Modulus = modulus;
+            rsaKeyInfo.Exponent = exponent;
+
+            rsa.ImportParameters(rsaKeyInfo);
+
+            Aes aes = Aes.Create();
+            encryptedSymmetricKey = rsa.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
+            encryptedSymmetricIV = rsa.Encrypt(aes.IV, RSAEncryptionPadding.Pkcs1);
+        }
+    }
+
+    private static void OpenAccount(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму для создания счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Выберите тип счета: 1. До востребования 2. Депозит");
+            AccountType accountType;
+
+            int type = Convert.ToInt32(Console.ReadLine());
+
+            if (type == 2)
+                accountType = AccountType.Deposit;
+            else
+                accountType = AccountType.Ordinary;
+
+            bank.Open(accountType,
+                sum,
+                AddSumHandler,  
+                WithdrawSumHandler, 
+                (o, e) => Console.WriteLine(e.Message), 
+                CloseAccountHandler, 
+                OpenAccountHandler); 
+        }
+
+        private static void Withdraw(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму для вывода со счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Withdraw(sum, id);
+        }
+
+        private static void Put(Bank<Account> bank)
+        {
+            Console.WriteLine("Укажите сумму, чтобы положить на счет:");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите Id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            bank.Put(sum, id);
+        }
+
+        private static void CloseAccount(Bank<Account> bank)
+        {
+            Console.WriteLine("Введите id счета, который надо закрыть:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Close(id);
+        }
+      
+        private static void OpenAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+        private static void AddSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private static void CloseAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        private static void WithdrawSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            if (e.Sum > 0)
+                Console.WriteLine("Идем тратить деньги");
+        }
+       
+       
+    }
+}
