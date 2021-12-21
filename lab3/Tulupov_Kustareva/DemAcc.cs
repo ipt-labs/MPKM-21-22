43d2512b5a10465a2554a3573faf6128521e8926
squareKM <90339117+squareKM@users.noreply.github.com>
2021-12-22 00:36:56 +0200

    Add files via upload

---
+++ lab3/Tulupov_Kustareva/DemAcc.cs
@@ -0,0 +1,12 @@
+﻿using Library;
+public class DemAccount : Account
+{
+    public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+    {
+    }
+
+    protected internal override void Open()
+    {
+        base.OnOpened(new AccountEventArgs($"Открыт новый счет! Id счета: {this.Id}", this.Sum));
+    }
+}
